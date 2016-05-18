eafaaad233aa29f7d9aecb5cd710ca05ecaf216a
seansquared <eashan.garg1@gmail.com>
2016-05-18 14:39:25 -0700

    SuperClass for a Card

---
+++ src/Card.py
@@ -0,0 +1,14 @@
+import abc
+
+class Card(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def type(self):
+        return
+
+    def cost(self):
+        return
+
+    def state(self):
+        return
