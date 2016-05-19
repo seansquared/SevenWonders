b9e33c2e6ede0e369a4c51b56b5fba5831dc3d8f
Eashan <eashan.garg1@gmail.com>
2016-05-18 18:20:34 -0700

    SuperClass for a Card

--- src/Card.py
+++ src/Card.py
@@ -1,16 +1,39 @@
 
-class Card(object):
+class Card():
 
-    def __init__(self, name, cost, state):
+    name = ""
+    cost = ""
+    state = 0
+    age = 0
+    player = 0
+
+    def __init__(self, name, age, player, cost, state):
         name = name.self
         cost = cost.self
         state = state.self
+        age = age.self
+        player = player.self
 
     def name(self, name):
         return name
+
+    def age(self, age):
+        return age
+
+    def player(self, player):
+        return player
 
     def cost(self, cost):
         return cost
 
     def state(self, state):
         return state
+
+
+###make this a Pyfunction to generate GCODE initial conditions
+file = open("geecode.txt", "w")
+file.write(";layer_height = 0.3\n")
+file.write(";perimeters = 4\n")
+file.write(";top_solid_layers = 2\n")
+file.write(";bottom_solid_laters = 3\n")
+file.write(";fill_density = 1\n")
--- src/CardList.py
+++ src/CardList.py
@@ -1,1 +1,92 @@
-class CardList:
+import Card
+class CardList():
+
+    resource_cards = []
+
+    resource_cards.append(Card("Lumber Yard", 1, 3, "none", 0))
+    resource_cards.append(Card("Stone Pit", 1, 3, "none", 0))
+    resource_cards.append(Card("Clay Pool", 1, 3, "none", 0))
+    resource_cards.append(Card("Ore Vein", 1, 3, "none", 0))
+    resource_cards.append(Card("Clay Pit", 1, 3, "One Gold", 0))
+    resource_cards.append(Card("Timber Yard", 1, 3, "One Gold", 0))
+    resource_cards.append(Card("Lumber Yard", 1, 4, "none", 0))
+    resource_cards.append(Card("Ore Vein", 1, 4, "none", 0))
+    resource_cards.append(Card("Excavation", 1, 4, "One Gold", 0))
+    resource_cards.append(Card("Stone Pit", 1, 5, "none", 0))
+    resource_cards.append(Card("Clay Pool", 1, 5, "none", 0))
+    resource_cards.append(Card("Forest Cave", 1, 5, "One Gold", 0))
+    resource_cards.append(Card("Tree Farm", 1, 6, "One Gold", 0))
+    resource_cards.append(Card("Mine", 1, 6, "One Gold", 0))
+    resource_cards.append(Card("Sawmill", 2, 3, "One Gold", 0))
+    resource_cards.append(Card("Quarry", 2, 3, "One Gold", 0))
+    resource_cards.append(Card("Brickyard", 2, 3, "One Gold", 0))
+    resource_cards.append(Card("Foundry", 2, 3, "One Gold", 0))
+    resource_cards.append(Card("Sawmill", 2, 4, "One Gold", 0))
+    resource_cards.append(Card("Quarry", 2, 4, "One Gold", 0))
+    resource_cards.append(Card("Brickyard", 2, 4, "One Gold", 0))
+    resource_cards.append(Card("Foundry", 2, 4, "One Gold", 0))
+
+    manufactured_cards = []
+    manufactured_cards.append(Card("Loom", 1, 3, "none", 0))
+    manufactured_cards.append(Card("Glassworks", 1, 3, "none", 0))
+    manufactured_cards.append(Card("Press", 1, 3, "none", 0))
+    manufactured_cards.append(Card("Loom", 1, 6, "none", 0))
+    manufactured_cards.append(Card("Glassworks", 1, 6, "none", 0))
+    manufactured_cards.append(Card("Press", 1, 6, "none", 0))
+    manufactured_cards.append(Card("Loom", 2, 3, "none", 0))
+    manufactured_cards.append(Card("Glassworks", 2, 3, "none", 0))
+    manufactured_cards.append(Card("Press", 2, 3, "none", 0))
+    manufactured_cards.append(Card("Loom", 2, 5, "none", 0))
+    manufactured_cards.append(Card("Glassworks", 2, 5, "none", 0))
+    manufactured_cards.append(Card("Press", 2, 5, "none", 0))
+
+    civilian_cards = []
+    civilian_cards.append(Card("Baths", 1, 3, "One Stone", 0))
+    civilian_cards.append(Card("Altar", 1, 3, "none", 0))
+    civilian_cards.append(Card("Theater", 1, 3, "none", 0))
+    civilian_cards.append(Card("Pawnshop", 1, 4, "none", 0))
+    civilian_cards.append(Card("Altar", 1, 5, "none", 0))
+    civilian_cards.append(Card("Theater", 1, 6, "none", 0))
+    civilian_cards.append(Card("Pawnshop", 1, 7, "none", 0))
+    civilian_cards.append(Card("Baths", 1, 7, "One Stone", 0))
+    civilian_cards.append(Card("Aqueduct", 2, 3, "Three Stone", 0))
+    civilian_cards.append(Card("Temple", 2, 3, "One Wood, One Clay, One Glass", 0))
+    civilian_cards.append(Card("Statue", 2, 3, "One Wood, Two Ore", 0))
+    civilian_cards.append(Card("Temple", 2, 6, "One Wood, One Clay, One Glass", 0))
+    civilian_cards.append(Card("Aqueduct", 2, 7, "Three Stone", 0))
+    civilian_cards.append(Card("Statue", 2, 7, "One Wood, Two Ore", 0))
+    civilian_cards.append(Card("Pantheon", 3, 3, "Two Clay, One Ore, One Papyrus, One Loom, One Glass", 0))
+    civilian_cards.append(Card("Gardens", 3, 3, "One Wood, Two Clay", 0))
+    civilian_cards.append(Card("Town Hall", 3, 3, "One Glass, One Ore, Two Stone", 0))
+    civilian_cards.append(Card("Palace", 3, 3, "One Glass, One Papyrus, One Loom, One Clay, One Wood, One Ore, One Stone", 0))
+    civilian_cards.append(Card("Gardens", 3, 4, "One Wood, Two Clay", 0))
+    civilian_cards.append(Card("Town Hall", 3, 5, "One Glass, One Ore, Two Stone", 0))
+    civilian_cards.append(Card("Pantheon", 3, 6, "Two Clay, One Ore, One Papyrus, One Loom, One Glass", 0))
+    civilian_cards.append(Card("Town Hall", 3, 6, "One Glass, One Ore, Two Stone", 0))
+    civilian_cards.append(Card("Palace", 3, 7, "One Glass, One Papyrus, One Loom, One Clay, One Wood, One Ore, One Stone", 0))
+
+    commerce_cards = []
+    commerce_cards.append(Card("East Trading Post", 1, 3, "none", 0))
+    commerce_cards.append(Card("West Trading Post", 1, 3, "none", 0))
+    commerce_cards.append(Card("Marketplace", 1, 3, "none", 0))
+    commerce_cards.append(Card("Tavern", 1, 4, "none", 0))
+    commerce_cards.append(Card("Tavern", 1, 5, "none", 0))
+    commerce_cards.append(Card("Marketplace", 1, 6, "none", 0))
+    commerce_cards.append(Card("Tavern", 1, 7, "none", 0))
+    commerce_cards.append(Card("East Trading Post", 1, 7, "none", 0))
+    commerce_cards.append(Card("West Trading Post", 1, 7, "none", 0))
+    commerce_cards.append(Card("Forum", 2, 3, "Two Clay", 0))
+    commerce_cards.append(Card("Caravansery", 2, 3, "Two Wood", 0))
+    commerce_cards.append(Card("Vineyard", 2, 3, "none", 0))
+    commerce_cards.append(Card(""))
+
+
+
+
+
+
+
+
+
+
+
