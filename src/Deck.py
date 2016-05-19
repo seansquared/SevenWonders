da2ee3e648da2b54d1fb1b26712ab3d912653d9a
Eashan <eashan.garg1@gmail.com>
2016-05-19 15:17:44 -0700

    Shuffles deck, need to create hand now ###FINISHEDTODO filter cards by players

--- src/Deck.py
+++ src/Deck.py
@@ -3,36 +3,34 @@ import random
 
 class Deck:
 
-    hand = []
-
     def __init__(self, age, player):
         self.age = age
         self.player = player
 
-    def shuffle(age):
+    def shuffle(age, player):
         age_deck = []
         for x in range(0, (len(CardList.resource_cards))):
-            if (CardList.resource_cards[x].age == age):
+            if ((CardList.resource_cards[x].age == age) and (CardList.resource_cards[x].player <= player)):
                 age_deck.append(CardList.resource_cards[x])
 
         for x in range(0, (len(CardList.manufactured_cards))):
-            if (CardList.manufactured_cards[x].age == age):
+            if ((CardList.manufactured_cards[x].age == age) and (CardList.manufactured_cards[x].player <= player)):
                 age_deck.append(CardList.manufactured_cards[x])
 
         for x in range(0, (len(CardList.civilian_cards))):
-            if (CardList.civilian_cards[x].age == age):
+            if ((CardList.civilian_cards[x].age == age) and (CardList.civilian_cards[x].player <= player)):
                 age_deck.append(CardList.civilian_cards[x])
 
         for x in range(0, (len(CardList.commerce_cards))):
-            if (CardList.commerce_cards[x].age == age):
+            if ((CardList.commerce_cards[x].age == age) and (CardList.commerce_cards[x].player <= player)):
                 age_deck.append(CardList.commerce_cards[x])
 
         for x in range(0, (len(CardList.military_cards))):
-            if (CardList.military_cards[x].age == age):
+            if ((CardList.military_cards[x].age == age) and (CardList.military_cards[x].player <= player)):
                 age_deck.append(CardList.military_cards[x])
 
         for x in range(0, (len(CardList.science_cards))):
-            if (CardList.science_cards[x].age == age):
+            if ((CardList.science_cards[x].age == age) and (CardList.science_cards[x].player <= player)):
                 age_deck.append(CardList.science_cards[x])
 
         age_deck = random.sample(age_deck, len(age_deck))
@@ -45,16 +43,18 @@ class Deck:
         hand = []
         for x in range(0, 7):
             hand.append(deck[x])
-            del deck[x]
-
         return hand
 
-    ay = shuffle(1)
+    ay = shuffle(1, 3)
     handrwef = hand(ay, 7)
+    for x in range(0,7):
+        del ay[x]
     handrwef = hand(ay, 7)
+    for x in range(0, 7):
+        del ay[x]
     handrwef = hand(ay, 7)
-    handrwef = hand(ay, 7)
-
+    for x in range(0, 7):
+        del ay[x]
     print(handrwef)
     print(ay)
 
